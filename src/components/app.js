5859d90892e15f5260bd14f66fb960caf69371ea
Bew Harichanwong <bew.hari@gmail.com>
2016-09-22 16:20:41 -0500

    Add static google-finance call

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,32 @@
 import React from 'react'
+var googleFinance = require('google-finance')
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount () {
+    googleFinance.historical({
+      symbol: 'NASDAQ:AAPL',
+      from: '2016-09-20',
+      to: '2016-09-21'
+    }, function(err, quotes) {
+      if (err) {
+        throw err
+      }
+
+      this.setState({data: quotes})
+    }.bind(this))
+  }
+
   render () {
     return (
       <div>
-        Hello, world!
+        { JSON.stringify(this.state.data) }
       </div>
     )
   }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -34,6 +34,8 @@ module.exports = {
       { test: /\.scss?$/,
         loader: 'style!css!sass',
         include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.json$/,
+        loader: 'json-loader' },
       { test: /\.png$/,
         loader: 'file' },
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
